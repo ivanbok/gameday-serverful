264b5b4d62b184276f4ca99f3873e9e1dc4512de
Ivan Bok <bokivan@hotmail.com>
2022-07-01 14:15:48 +0800

    Removed migration file

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@ python3 -m venv env
 source env/bin/activate
 python -m pip install django
 python -m pip freeze > requirements.txt
+
+## For Amazon Linux 2
+sudo yum install python3-devel mysql-devel
+pip install mysqlclient
